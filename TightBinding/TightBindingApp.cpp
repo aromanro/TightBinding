ff0fe9dcd5fc368ad00ddf418803c19d825eb90e
Adrian Roman <aroman_ro@yahoo.com>
2018-09-05 20:14:35 +0300

    Call OnInit from application base class

--- TightBinding/TightBindingApp.cpp
+++ TightBinding/TightBindingApp.cpp
@@ -7,6 +7,9 @@ wxIMPLEMENT_APP(TightBindingApp);
 
 bool TightBindingApp::OnInit()
 {
+	if (!wxApp::OnInit())
+		return false;
+
 	frame = new TightBindingFrame("TightBinding", wxPoint(50, 50), wxSize(1024, 800));
 	frame->Show(true);
 
