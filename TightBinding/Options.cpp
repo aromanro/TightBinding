31da9917101be7c9751eae32332d0acbf3bd2497
Adrian Roman <aroman_ro@yahoo.com>
2019-11-10 11:44:17 +0200

    Added more paths

--- TightBinding/Options.cpp
+++ TightBinding/Options.cpp
@@ -5,7 +5,16 @@
 
 Options::Options()
 	: nrThreads(2), materialName("GaP"), nrPoints(600), pathNo(0),
-	paths{ { {"K", "W", "X", "G", "L", "W"}, {"L", "G", "X", "K", "G" }, {"W", "G", "X", "W", "L", "G"}, {"L", "G", "X", "W", "K", "G"}, {"L", "G", "X", "U", "K", "G"}, {"G", "X", "K", "G", "L", "K", "W", "X"}, {"G", "X", "W", "L", "G", "K", "W", "U"} } },
+	paths{ { {"K", "W", "X", "G", "L", "W"}, 
+			 {"L", "G", "X", "K", "G" }, 
+			 {"W", "G", "X", "W", "L", "G"}, 
+			 {"L", "G", "X", "W", "K", "G"}, 
+			 {"L", "G", "X", "U", "K", "G"}, 
+			 {"G", "X", "K", "G", "L", "K", "W", "X"}, 
+			 {"G", "X", "W", "L", "G", "K", "W", "U"}, 
+			 {"G", "X", "W", "L", "G", "K"},
+			 {"G", "X", "W", "G", "U", "X"}
+		} },
 	m_fileconfig(nullptr)
 {
 }
